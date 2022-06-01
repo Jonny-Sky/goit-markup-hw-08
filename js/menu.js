d05da0d4e19b53ab77eb6ebd86227de583c57bc2
Jonny-Sky <evgen2873202@gmail.com>
2022-06-02 00:32:09 +0300

    завершает работу

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,14 @@
     openMenuBtn: document.querySelector(".mob-menu-btn"),
     closeMenuBtn: document.querySelector(".mob-close-btn"),
     menu: document.querySelector(".mob-menu"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("is-open");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
